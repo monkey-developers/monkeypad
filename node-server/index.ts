f7fdb323c1762d670cdb886bfbf0d3b50e5ac59b
abehidek <hidek.abe@outlook.com>
2022-06-28 18:26:18 -0300

    Add prisma/socket.io/body-parser and create api routes

--- node-server/index.ts
+++ node-server/index.ts
@@ -1,14 +1,39 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+// import { Server } from 'socket.io'
+import { getAllPages, getPage, upsertPage } from './prisma/page';
+import bodyParser, { BodyParser } from 'body-parser'
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Express + TypeScript Server');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+
+app.get('/', async (req: Request, res: Response) => {
+    const pages = await getAllPages();
+    res.send(pages);
 });
+
+app.get("/:page", async (req: Request, res: Response) => {
+    const name = req.params.page;
+    const page = await getPage(name);
+    res.send(page)
+})
+
+app.post('/:page', async (req: Request, res: Response) => {
+    const name = req.params.page;
+    let body = "";
+
+    if (Object.getOwnPropertyNames(req.body).includes("body")) {
+        body = req.body.body;
+    }
+
+    const page = await upsertPage(name, body);
+    res.send(page);
+})
 
 app.listen(port, () => {
     console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
---
+++ node-server/prisma/page.ts
@@ -0,0 +1,35 @@
+import prisma from ".";
+
+export const getAllPages = async () => {
+    const pages = await prisma.page.findMany();
+    return pages;
+}
+
+export const getPage = async (name: string) => {
+    let page = await prisma.page.findFirst({
+        where: { name: name },
+    });
+    if (!page) {
+        page = await prisma.page.create({
+            data: {
+                body: "",
+                name: name
+            }
+        });
+    }
+    return page;
+}
+
+export const upsertPage = async (name: string, body: string) => {
+    const page = await prisma.page.upsert({
+        where: { name: name, },
+        update: {
+            body: body,
+        },
+        create: {
+            name: name,
+            body: body
+        }
+    });
+    return page;
+}
