30675489949b3911ebbec4589df1488fa4792673
abehidek <hidek.abe@outlook.com>
2022-06-28 16:46:17 -0300

    Create basic actix web server

--- actix-server/src/main.rs
+++ actix-server/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, web, App, HttpServer, Responder};
+
+#[get("/hello/{name}")]
+async fn greet(name: web::Path<String>) -> impl Responder {
+    format!("Hello {name}")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/hello", web::get().to(|| async { "Hello World" }))
+            .service(greet)
+    })
+    .bind(("127.0.0.1", 8000))?
+    .run()
+    .await
 }
