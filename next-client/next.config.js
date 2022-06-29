ce49fb5550be2072dd9b5c2f093166061a471de8
abehidek <hidek.abe@outlook.com>
2022-06-29 19:13:53 -0300

    Update nextjs CSP

--- next-client/next.config.js
+++ next-client/next.config.js
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
+
+const securityHeaders = [
+  {
+    key: 'Content-Security-Policy',
+    value: "upgrade-insecure-requests"
+  }
+]
+
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
