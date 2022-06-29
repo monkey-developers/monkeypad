27a323ad1b01748cde10c17f42b4cd5771d0eaf1
abehidek <hidek.abe@outlook.com>
2022-06-29 19:22:41 -0300

    Update cors policy

--- node-server/index.ts
+++ node-server/index.ts
@@ -14,7 +14,7 @@ const server = http.createServer(app)
 
 const io = new Server(server, {
     cors: {
-        origin: process.env.CLIENT_ORIGIN,
+        origin: "*",
         methods: ["GET", "POST"],
     },
 });
