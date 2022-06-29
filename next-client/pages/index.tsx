34926a7d9cee66bec70b40e5ef897f7491d54401
abehidek <hidek.abe@outlook.com>
2022-06-29 20:22:17 -0300

    Add monkey and input enter key listener

--- next-client/pages/index.tsx
+++ next-client/pages/index.tsx
@@ -1,18 +1,25 @@
 import type { NextPage } from 'next'
 import { useRouter } from 'next/router'
+import Image from 'next/image'
 import { useState } from 'react'
+import monkeyPic from '../public/monkey.jpg'
 
 const Home: NextPage = () => {
   const [pageName, setPageName] = useState("");
   const router = useRouter()
 
-  const handleInput = (event: any) => { setPageName(event.target.value); }
+  const handleInput = (event: any) => { setPageName(event.target.value); };
+
+  const handleKeyDown = (event: any) => {
+    if (event.key === 'Enter') { router.push(pageName) }
+  };
 
   return (
     <main className='flex items-center flex-col h-screen justify-center bg-slate-200 gap-5'>
+      <Image src={monkeyPic} alt="Picture of a Monkey" width={300} height={300} />
       <h1 className='text-3xl font-bold'>Welcome to Monkeypad!</h1>
       <p className='text-1xl'>Which note do you want to select? :</p>
-      <input onChange={handleInput} type="text" className='shadow appearance-none border rounded w-96 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline' />
+      <input onKeyDown={handleKeyDown} onChange={handleInput} type="text" className='shadow appearance-none border rounded w-96 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline' />
       <button onClick={() => router.push(pageName)} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline' >
         Enter
       </button>
