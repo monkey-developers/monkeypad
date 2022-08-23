e71c70be0ffd4acf0f793c1cdc5f38815913bf38
abehidek <hidek.abe@outlook.com>
2022-08-22 21:40:43 -0300

    Change dev env db name

--- monkeypad/config/dev.exs
+++ monkeypad/config/dev.exs
@@ -5,7 +5,7 @@ config :monkeypad, Monkeypad.Repo,
   username: "postgres",
   password: "postgres",
   hostname: "localhost",
-  database: "monkeypad",
+  database: "postgres",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
